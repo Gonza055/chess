616333d91c72a9286b886d1bb788a40b51838aca
Gonzalo <gloayza05@gmail.com>
2024-01-26 06:24:11 -0700

    Fixed an indexing issue

--- chessx/shared/src/main/java/chess/ChessPiece.java
+++ chessx/shared/src/main/java/chess/ChessPiece.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 
 public class ChessPiece {
     private final PieceType type;
-
+    private final ChessGame.TeamColor pieceColor;
     public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
         this.type = type;
+        this.pieceColor = pieceColor;
+
     }
 
 
@@ -25,7 +27,7 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return this.pieceColor;
     }
 
     /**
@@ -35,6 +37,17 @@ public class ChessPiece {
         return this.type;
     }
 
+
+
+    public boolean isOccupied(ChessPosition position) {
+        int row = position.getRow() - 1; // Adjust for 0-indexed array if needed
+        int col = position.getColumn() - 1; // Adjust for 0-indexed array if needed
+        return board[row][col] == null; // Returns true if there's a piece at the position
+    }
+
+
+
+
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -43,7 +56,8 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        ArrayList moves=new ArrayList();
+        ArrayList moves;
+        moves=new ArrayList();
         ChessPiece myPiece=board.getPiece(myPosition);
         int row=myPosition.getRow();
         int col=myPosition.getColumn();
@@ -57,20 +71,29 @@ public class ChessPiece {
                 }
             }
         } else if (myPiece.getPieceType() == PieceType.BISHOP) {
-            ChessPosition startPosition = new ChessPosition(row, col);
-            int[][] directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+            ChessPosition startPosition=new ChessPosition(row, col);
+            int[][] directions={{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
             for (int[] direction : directions) {
-                for (int i = 1; i <= 8; i++) {
-                    int newRow = row + i * direction[0];
-                    int newCol = col + i * direction[1];
+                for (int i=1; i <= 8; i++) {
+                    int newRow=row + i * direction[0];
+                    int newCol=col + i * direction[1];
+                    ChessPosition endPosition=new ChessPosition(newRow, newCol);
                     if (newRow >= 1 && newRow <= 8 && newCol >= 1 && newCol <= 8) {
-                        ChessPosition endPosition = new ChessPosition(newRow, newCol);
-                        moves.add(new ChessMove(startPosition, endPosition, myPiece.getPieceType()));
+                        //if (board.isOccupied(endPosition)) {
+                           // if (endPosition.isEnemy(endPosition, myPiece.getTeamColor())) {
+                                moves.add(new ChessMove(startPosition, endPosition, myPiece.getPieceType()));
+                            }
+                            break;
+                        } //else {
+                            moves.add(new ChessMove(startPosition, endPosition, myPiece.getPieceType()));
+                        }
+                    } else {
+                        break;
                     }
                 }
             }
-
-    }return moves;
+        }
+    return moves;
 }}
 
 
