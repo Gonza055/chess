285516f100e517512cc2f5b350006fcc2399a66f
Gonzalo <gloayza05@gmail.com>
2024-03-09 22:30:07 -0700

    Cleanup

--- server/src/main/java/dataAccess/DatabaseManager.java
+++ server/src/main/java/dataAccess/DatabaseManager.java
@@ -10,9 +10,7 @@ public class DatabaseManager {
     private static final String password;
     private static final String connectionUrl;
 
-    /*
-     * Load the database information for the db.properties file.
-     */
+
     static {
         try {
             try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties")) {
@@ -31,23 +29,14 @@ public class DatabaseManager {
             throw new RuntimeException("unable to process db.properties. " + ex.getMessage());
         }
     }
-
-    /**
-     * Creates the database if it does not already exist.
-     */
     public static void createDatabase() throws DataAccessException {
         try {
-            // Create database
             var statement = "CREATE DATABASE IF NOT EXISTS " + databaseName;
             var conn = DriverManager.getConnection(connectionUrl, user, password);
             try (var preparedStatement = conn.prepareStatement(statement)) {
                 preparedStatement.executeUpdate();
             }
-
-            // Access the database
             conn.setCatalog(databaseName);
-
-            // Create games table
             String createGamesTable = "CREATE TABLE IF NOT EXISTS games (" +
                     "ID INT NOT NULL, " +
                     "gameID INT NOT NULL PRIMARY KEY, " +
@@ -58,8 +47,6 @@ public class DatabaseManager {
             try (var preparedStatement = conn.prepareStatement(createGamesTable)) {
                 preparedStatement.executeUpdate();
             }
-
-            // Create users table
             String createUsersTable = "CREATE TABLE IF NOT EXISTS users (" +
                     "ID INT NOT NULL PRIMARY KEY, " +
                     "username VARCHAR(255), " +
@@ -68,8 +55,6 @@ public class DatabaseManager {
             try (var preparedStatement = conn.prepareStatement(createUsersTable)){
                 preparedStatement.executeUpdate();
             }
-
-            // Create auth table
             String createAuthTable = "CREATE TABLE IF NOT EXISTS auth (" +
                     "ID INT NOT NULL PRIMARY KEY, " +
                     "authToken VARCHAR(255), " +
@@ -77,24 +62,11 @@ public class DatabaseManager {
             try (var preparedStatement = conn.prepareStatement(createAuthTable)){
                 preparedStatement.executeUpdate();
             }
-
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
         }
     }
 
-    /**
-     * Create a connection to the database and sets the catalog based upon the
-     * properties specified in db.properties. Connections to the database should
-     * be short-lived, and you must close the connection when you are done with it.
-     * The easiest way to do that is with a try-with-resource block.
-     * <br/>
-     * <code>
-     * try (var conn = DbInfo.getConnection(databaseName)) {
-     * // execute SQL statements.
-     * }
-     * </code>
-     */
     static Connection getConnection() throws DataAccessException {
         try {
             var conn = DriverManager.getConnection(connectionUrl, user, password);
