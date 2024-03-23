cc0284b8206391a8669a3b8fcab556abf775c897
Gonzalo <gloayza05@gmail.com>
2024-03-22 23:39:22 -0600

    Initial code, plus advancement of client tests

---
+++ client/src/main/java/ui/Client.java
@@ -0,0 +1,169 @@
+package ui;
+
+import model.GameData;
+import chess.ChessBoard;
+import response.CreateGameResponse;
+import response.JoinGameResponse;
+import response.ListGamesResponse;
+
+import javax.swing.tree.AbstractLayoutCache;
+import java.awt.*;
+import java.util.Scanner;
+import java.util.List;
+
+public class Client {
+
+    public static void main(String[] args) throws Exception {
+        ServerFacade serverFacade = new ServerFacade("http://localhost", Integer.parseInt(args[0]));
+        System.out.println("Welcome to CS 240 Chess\nType 'help' to get started");
+        int userState = 0;
+        boolean isPlayer = false;
+        Scanner userInput = new Scanner(System.in);
+        String defaultColor = "\u001B[0m";
+        String blueColor = "\u001B[34m";
+        while (true) {
+            switch (userState) {
+                case 0:
+                    String preInput = userInput.nextLine();
+                    String[] splitPreInput = preInput.split("\\s+");
+                    if (splitPreInput[0].equals("register") && splitPreInput.length == 4) {
+                        String username = splitPreInput[1];
+                        String password = splitPreInput[2];
+                        String email = splitPreInput[3];
+                        try {
+                            serverFacade.register(username, password, email);
+                            System.out.println("Logged in as " + blueColor + username + defaultColor);
+                            System.out.println("Type 'help' for a list of available options.");
+                            userState = 1;
+                        } catch (Exception e) {
+                            System.out.println("Failed to register. Be sure you have a unique username.");
+                        }
+                    }
+                    else if (splitPreInput[0].equals("login") && splitPreInput.length == 3) {
+                        String username = splitPreInput[1];
+                        String password = splitPreInput[2];
+                        try {
+                            serverFacade.login(username, password);
+                            System.out.println("Logged in as " + blueColor + username + defaultColor);
+                            System.out.println("Type 'help' for a list of available options.");
+                            userState = 1;
+                        } catch (Exception e) {
+                            System.out.println("Failed to log in. Check your credentials and try again.");
+                        }
+                    }
+                    else if (splitPreInput[0].equals("quit")) {
+                        System.out.println("Exiting... Goodbye!");
+                        return;
+                    }
+                    else if (splitPreInput[0].equals("help")) {
+                        System.out.println("register <USERNAME> <PASSWORD> <EMAIL>" + blueColor + " - Register a new user" + defaultColor);
+                        System.out.println("login <USERNAME> <PASSWORD>" + blueColor + " - Login with existing credentials" + defaultColor);
+                        System.out.println("quit" + blueColor + " - Exit the game" + defaultColor);
+                        System.out.println("help" + blueColor + " - List available commands" + defaultColor);
+                    }
+                    else {
+                        System.out.println("ERROR: Unknown command or incorrect syntax. Please type 'help' for a list of commands");
+                    }
+                    break;
+                case 1:
+                    String postInput = userInput.nextLine();
+                    String[] splitPostInput = postInput.split("\\s+");
+                    if (splitPostInput[0].equals("create") && splitPostInput.length == 2) {
+                        String gameName = splitPostInput[1];
+                        try {
+                            CreateGameResponse response = serverFacade.create(gameName);
+                            System.out.println("Successfully created game " + blueColor + gameName + defaultColor);
+                        } catch (Exception e) {
+                            System.out.println("Failed to create game. Be sure you gave the game a unique name. Please type 'help' for a list of commands.");
+                        }
+                    }
+                    else if (splitPostInput[0].equals("list")) {
+                        try {
+                            ListGamesResponse response = serverFacade.list();
+                            System.out.println("Available Games\n----------------");
+                            List<GameData> gameList = response.getGames();
+
+                            int listNum = 1;
+                            for (GameData game : gameList) {
+
+                                System.out.println(listNum + ". Game ID - " + blueColor + game.gameID() + defaultColor + " | Game Name - " +
+                                       blueColor + game.gameName() + defaultColor + " | White Player - " + blueColor +
+                                        game.whiteUsername() + defaultColor + " | Black Player - " + blueColor + game.blackUsername() +
+                                        defaultColor + " |");
+                                listNum++;
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Couldn't retrieve list of games. Please type 'help' for a list of commands.");
+                        }
+                    }
+                    else if (splitPostInput[0].equals("join") && splitPostInput.length == 3) {
+                        int gameID = Integer.parseInt(splitPostInput[1]);
+                        String teamColor = splitPostInput[2];
+                        try {
+                            JoinGameResponse response = serverFacade.join(teamColor, gameID);
+                            ChessBoard board = response.game.getBoard();
+                            board.resetBoard();
+                            System.out.println(board.toString(teamColor));
+                            isPlayer = true;
+                            userState = 2;
+                        } catch (Exception e){
+                            System.out.println("Failed to join. Be sure you picked a valid Game ID. The game could be full, or the team color you specified is taken. Please type 'help' for a list of commands.");
+                        }
+                    }
+                    else if ((splitPostInput[0].equals("observe") && splitPostInput.length == 2) || (splitPostInput[0].equals("join") && splitPostInput.length == 2)) {
+                        int gameID = Integer.parseInt(splitPostInput[1]);
+
+                        try {
+                            JoinGameResponse response =  serverFacade.join(null, gameID);
+                            ChessBoard board = response.game.getBoard();
+                            board.resetBoard();
+                            System.out.println(board.toString("WHITE"));
+                            userState = 2;
+                        } catch (Exception e){
+                            System.out.println("Failed to observe. Please type 'help' for a list of commands.");
+                        }
+                    }
+                    else if (splitPostInput[0].equals("logout")) {
+                        try {
+                            serverFacade.logout();
+                            System.out.println("Successfully logged out.");
+                            System.out.println("Type 'help' to get started.");
+                            userState = 0;
+                        } catch (Exception e){
+                            System.out.println("Failed to log out. Please type 'help' for a list of commands.");
+                        }
+                    }
+                    else if (splitPostInput[0].equals("quit")) {
+                        System.out.println("Exiting... Goodbye!");
+                        return;
+                    }
+                    else if (splitPostInput[0].equals("help")) {
+                        System.out.println("create <GAME_NAME>" + blueColor + " - Create a game" + defaultColor);
+                        System.out.println("list" + blueColor + " - Show available games" + defaultColor);
+                        System.out.println("join <ID> [WHITE|BLACK|<empty>]" + blueColor + " - Join game by ID" + defaultColor);
+                        System.out.println("observe <ID>" + blueColor + " - Watch a game" + defaultColor);
+                        System.out.println("logout" + blueColor + " - Logout" + defaultColor);
+                        System.out.println("quit" + blueColor + " - Exit the game" + defaultColor);
+                        System.out.println("help" + blueColor + " - List available commands" + defaultColor);
+                    }
+                    else {
+                        System.out.println("ERROR: Unknown command or incorrect syntax. Please type 'help' for a list of commands.");
+                    }
+                    break;
+
+                 case 2:
+                    String gameInput = userInput.nextLine();
+                    String[] splitGameInput = gameInput.split("\\s+");
+
+                    if (splitGameInput[0].equals("quit")) {
+                        System.out.println("Exiting... Goodbye!");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("Unknown state error occurred. Exiting...");
+                    return;
+            }
+        }
+    }
+}
---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,113 @@
+package ui;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.*;
+
+import request.*;
+import response.*;
+
+public class ServerFacade {
+    private final String serverUrl;
+    private String authToken;
+    private int gameID;
+
+    public ServerFacade(String url, int port) {
+        serverUrl = url + ":" + port;
+    }
+
+    public ClearResponse clearDatabase() throws Exception {
+        var path = "/db";
+        return this.makeRequest("DELETE", path, null, ClearResponse.class, null);
+    }
+
+    public RegisterResponse register(String username, String password, String email) throws Exception {
+        var path = "/user";
+        RegisterRequest registerRequest = new RegisterRequest(username, password, email);
+        RegisterResponse retResponse = this.makeRequest("POST", path, registerRequest, RegisterResponse.class, null);
+        authToken = retResponse.authToken;
+        return retResponse;
+    }
+
+    public LoginResponse login(String username, String password) throws Exception {
+        var path = "/session";
+        LoginRequest loginRequest = new LoginRequest(username, password);
+        LoginResponse retResponse =  this.makeRequest("POST", path, loginRequest, LoginResponse.class, authToken);
+        authToken = retResponse.authToken;
+        return retResponse;
+    }
+
+    public LogoutResponse logout() throws Exception {
+        var path = "/session";
+        return this.makeRequest("DELETE", path, null, LogoutResponse.class, authToken);
+    }
+
+    public ListGamesResponse list() throws Exception {
+        var path = "/game";
+        return this.makeRequest("GET", path, null, ListGamesResponse.class, authToken);
+    }
+
+    public CreateGameResponse create(String gameName) throws Exception {
+        var path = "/game";
+        CreateGameRequest createGameRequest = new CreateGameRequest(gameName);
+        return this.makeRequest("POST", path, createGameRequest, CreateGameResponse.class, authToken);
+    }
+    
+    public JoinGameResponse join(String playerColor, int gameID) throws Exception {
+        var path = "/game";
+        JoinGameRequest joinGameRequest = new JoinGameRequest(playerColor, gameID);
+        JoinGameResponse joinGameResponse = this.makeRequest("PUT", path, joinGameRequest, JoinGameResponse.class, authToken);
+        return  joinGameResponse;
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass, String authToken) throws Exception {
+        URL url = (new URI(serverUrl + path)).toURL();
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod(method);
+        http.setDoOutput(true);
+
+        if (authToken != null && !authToken.isEmpty()) {
+            http.setRequestProperty("authorization", authToken);
+        }
+
+        writeBody(request, http);
+        http.connect();
+        throwIfNotSuccessful(http);
+        return readBody(http, responseClass);
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws Exception {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new Exception("500 - Internal server error");
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
