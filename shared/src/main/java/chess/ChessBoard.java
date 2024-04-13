73a7cd0004fc436ac8b206d8455a082d95f87123
Gonzalo <gloayza05@gmail.com>
2024-04-12 23:23:23 -0600

    All done.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,7 +1,6 @@
 package chess;
-
-import java.util.Collection;
 import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * A chessboard that can hold and rearrange chess pieces.
@@ -95,13 +94,12 @@ public class ChessBoard implements Cloneable{
         return Arrays.deepEquals(board, that.board);
     }
 
-        String bgWhite = "\u001B[47m";
-        String bgBlack = "\u001B[40m";
-        String red = "\u001B[31m";
-        String blue = "\u001B[34m";
-        String reset = "\u001B[0m";
-        String black = "\u001B[30m";
-
+    String reset = "\u001B[0m";
+    String black = "\u001B[30m";
+    String red = "\u001B[31m";
+    String blue = "\u001B[34m";
+    String bgWhite = "\u001B[47m";
+    String bgBlack = "\u001B[40m";
 
     public String toString(String teamColor) {
         StringBuilder sb = new StringBuilder();
@@ -110,8 +108,6 @@ public class ChessBoard implements Cloneable{
 
         return sb.toString();
     }
-
-
     private StringBuilder buildBoard(String teamColor, StringBuilder sb, String reset, String black, String red, String blue, String bgWhite, String bgBlack) {
         sb.append(black).append(String.format("%4s", " ")).append("a  b  c  d  e  f  g  h").append(reset).append("\n");
 
@@ -128,8 +124,6 @@ public class ChessBoard implements Cloneable{
         sb.append(black).append(String.format("%4s", " ")).append("a  b  c  d  e  f  g  h").append(reset).append("\n");
         return sb;
     }
-
-
     public String toStringHighlighted(String teamColor, Collection<ChessMove> possibleMoves) {
         StringBuilder sb = new StringBuilder();
 
@@ -148,13 +142,52 @@ public class ChessBoard implements Cloneable{
         sb.append(black).append(String.format("%4s", " ")).append("a  b  c  d  e  f  g  h").append(reset).append("\n");
         return sb.toString();
     }
+    private StringBuilder appendHighlightedRow(StringBuilder sb, int i, String reset, String black, String red, String blue, String bgWhite, String bgBlack, Collection<ChessMove> possibleMoves) {
+        sb.append(black).append(String.format("%2d", i + 1)).append(" ");
+        for (int j = 0; j < 8; j++) {
+            ChessPosition currentPosition = new ChessPosition(i, j);
+            String bg = ((i + j) % 2 == 0) ? bgWhite : bgBlack;
+            if (isPossibleMove(currentPosition, possibleMoves)) {
+                sb.append("\u001B[32m");
+            }
+            if (board[i][j] == null) {
+                sb.append(bg).append(String.format("%3s", " ")).append(reset);
+            } else {
+                String color = (board[i][j].getTeamColor() == ChessGame.TeamColor.WHITE) ? red : blue;
+                char pieceChar;
+                switch (board[i][j].getPieceType()) {
+                    case PAWN: pieceChar = 'P'; break;
+                    case ROOK: pieceChar = 'R'; break;
+                    case KNIGHT: pieceChar = 'N'; break;
+                    case BISHOP: pieceChar = 'B'; break;
+                    case KING: pieceChar = 'K'; break;
+                    case QUEEN: pieceChar = 'Q'; break;
+                    default: pieceChar = ' '; break;
+                }
+                sb.append(bg).append(color).append(" ").append(pieceChar).append(" ").append(reset);
+            }
+            if (isPossibleMove(currentPosition, possibleMoves)) {
+                sb.append("\u001B[0m");
+            }
+        }
+        sb.append(black).append(" ").append(String.format("%2d", i + 1)).append(reset).append("\n");
 
+        return sb;
+    }
+    private boolean isPossibleMove(ChessPosition position, Collection<ChessMove> possibleMoves) {
+        for (ChessMove move : possibleMoves) {
+            if (move.getEndPosition().equals(position)) {
+                return true;
+            }
+        }
+        return false;
+    }
     private void appendRow(StringBuilder sb, int i, String reset, String black, String red, String blue, String bgWhite, String bgBlack) {
         sb.append(black).append(String.format("%2d", i + 1)).append(" ");
         for (int j = 0; j < 8; j++) {
             String bg = ((i + j) % 2 == 0) ? bgWhite : bgBlack;
             if (board[i][j] == null) {
-                sb.append(bg).append(String.format("%3s", " ")).append(reset); // Increased space for empty squares
+                sb.append(bg).append(String.format("%3s", " ")).append(reset);
             } else {
                 String color = (board[i][j].getTeamColor() == ChessGame.TeamColor.WHITE) ? red : blue;
                 char pieceChar;
@@ -172,7 +205,6 @@ public class ChessBoard implements Cloneable{
         }
         sb.append(black).append(" ").append(String.format("%2d", i + 1)).append(reset).append("\n");
     }
-
     @Override
     public ChessBoard clone() {
         try {
