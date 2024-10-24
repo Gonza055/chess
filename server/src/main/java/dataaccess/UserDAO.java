e9fbf62558a0e9405032a6f4c733b7424001db02
Gonza055 <gloayza05@gmail.com>
2024-10-23 21:20:28 -0600

    Implement methods in UserDAO

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
+import model.UserRecord;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserDAO {
+
+  private final Map<String, UserRecord> userRecords;
+
+  public UserDAO() {
+    userRecords = new HashMap<>();
+  }
+
+  public UserRecord findUser(String username) {
+    return userRecords.get(username);
+  }
+
+  public void createUser(UserRecord user) {
+    if (!userRecords.containsKey(user.info().username())) {
+      userRecords.put(user.info().username(), user);
+    }
+  }
+
+  public void deleteUsers() {
+    userRecords.clear();
+  }
 }
