f0eb8b7c9820afa7028f0e9fd4356e5dbd17fe2d
Gonza055 <gloayza05@gmail.com>
2024-10-23 20:47:25 -0600

    Implement AuthDAO and create exception class

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,38 @@
 package dataaccess;
 
+import model.AuthRecord;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class AuthDAO {
+
+  private final Map<String, AuthRecord> authRecords;
+
+  public AuthDAO() {
+    authRecords = new HashMap<>();
+  }
+
+  public AuthRecord createAuth(String username) {
+    String token = generateToken();
+    AuthRecord authRecord = new AuthRecord(token, username);
+    authRecords.put(token, authRecord);
+    return authRecord;
+  }
+
+  public AuthRecord getAuthByToken(String authToken) {
+    return authRecords.get(authToken);
+  }
+
+  public void deleteAuthByToken(String authToken) {
+    authRecords.remove(authToken);
+  }
+
+  public void clearAuthRecords() {
+    authRecords.clear();
+  }
+
+  private String generateToken() {
+    return UUID.randomUUID().toString();
+  }
 }
