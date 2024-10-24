6cf97f6f66a57f147a8208c1252e22d9d5f77e19
Gonza055 <gloayza05@gmail.com>
2024-10-23 21:33:01 -0600

    Can take StatusCode and ErrorDetails from exception now

--- server/src/main/java/exception/ResponseException.java
+++ server/src/main/java/exception/ResponseException.java
@@ -1,4 +1,33 @@
 package exception;
 
-public class ResponseException {
+public class ResponseException extends RuntimeException {
+
+  private final int statusCode;
+  private final String errorDetails;
+
+  public ResponseException(String message, int statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errorDetails = null;
+  }
+
+  public ResponseException(String message, int statusCode, String errorDetails) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errorDetails = errorDetails;
+  }
+
+  public int getStatusCode() {
+    return statusCode;
+  }
+
+  public String getErrorDetails() {
+    return errorDetails;
+  }
+
+  @Override
+  public String toString() {
+    String basicMessage = "ResponseException: " + getMessage() + " (Status Code: " + statusCode + ")";
+    return errorDetails != null ? basicMessage + " - Details: " + errorDetails : basicMessage;
+  }
 }
