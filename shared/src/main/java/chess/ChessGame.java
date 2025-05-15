e390f4265de9b15b5f803cdac11b3a9a8c1a9281
Gonzalo Loayza <gloayza05@gmail.com>
2025-05-14 21:22:18 -0500

    Made progress with isInStalemante

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -239,7 +239,33 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        if (!isInCheck(teamColor)) return false;
+        for (int row = 1; row <= 8; row++) {
+            for (int col = 1; col <= 8; col++) {
+                ChessPosition position = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(position);
+                if (piece != null && piece.getTeamColor() == teamColor) {
+                    Collection<ChessMove> moves = piece.pieceMoves(board, position);
+                    for (ChessMove move : moves) {
+                        ChessPiece targetPiece = board.getPiece(move.getEndPosition());
+                        ChessPiece pieceToPlace = piece;
+                        if (piece.getPieceType() == ChessPiece.PieceType.PAWN && move.getPromotionPiece() != null) {
+                            pieceToPlace = new ChessPiece(piece.getTeamColor(), move.getPromotionPiece());
+                        }
+                        board.addPiece(move.getEndPosition(), pieceToPlace);
+                        board.addPiece(position, null);
+
+                        boolean inCheckAfterMove = isInCheck(teamColor);
+                        board.addPiece(position, piece);
+                        board.addPiece(move.getEndPosition(), targetPiece);
+                        if (!inCheckAfterMove) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     /**
