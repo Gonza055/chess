e5f53c20a55631370a2f7b5992aa5f2bb963607b
Gonzalo Loayza <gloayza05@gmail.com>
2025-05-18 13:36:26 -0500

    Initial layout for Phase 3

---
+++ server/src/main/java/dataaccess/dataAccess.java
@@ -0,0 +1,23 @@
+package dataaccess;
+
+import chess.ChessGame;
+import model.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public interface dataAccess {
+    UserData getUser(String username) throws DataAccessException;
+    void createUser(UserData user) throws DataAccessException;
+    void createAuth(AuthData auth) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    GameData[] getAllGames() throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    void createGame(GameData game) throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+
+    void deleteAllUsers() throws DataAccessException;
+    void deleteAllAuth() throws DataAccessException;
+    void deleteAllGames() throws DataAccessException;
+}
