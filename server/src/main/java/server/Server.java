48771d943862391dd478cc1c6e77a262d34e2110
Gonzalo Loayza <gloayza05@gmail.com>
2025-05-21 18:25:35 -0500

    Implement route handlers

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,7 +1,11 @@
 package server;
 
+
+import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
 import dataaccess.dataAccess;
+import model.GameData;
+import org.eclipse.jetty.util.resource.EmptyResource;
 import service.*;
 import com.google.gson.Gson;
 import spark.*;
@@ -39,8 +43,168 @@ public class Server {
         return Spark.port();
     }
 
+    private void registerEndpoints() {
+        Spark.post("/user", (req, res) -> {
+            try {
+                RegisterRequest request = gson.fromJson(req.body(), RegisterRequest.class);
+                if (request == null || request.username == null || request.password == null || request.email == null) {
+                    res.status(400);
+                    return gson.toJson(new ErrorResponse("Error: Bad request"));
+                }
+                RegisterResult result = userService.register(request.username, request.password, request.email);
+                res.status(200);
+                return gson.toJson(result);
+            } catch (DataAccessException e) {
+                if (e.getMessage().equals("Username already exists")) {
+                    res.status(403);
+                    return gson.toJson(new ErrorResponse("Error: Already exists"));
+                }
+                res.status(500);
+                return gson.toJson(new ErrorResponse("Error: " + e.getMessage()));
+            }
+        });
+        Spark.post("/session", (req, res) -> {
+            try {
+                LoginRequest request = gson.fromJson(req.body(), LoginRequest.class);
+                if (request == null || request.username == null || request.password == null) {
+                    res.status(400);
+                    return gson.toJson(new ErrorResponse("Error: Bad Request"));
+                }
+                RegisterResult result = userService.login(request.username, request.password);
+                res.status(200);
+                return gson.toJson(result);
+            } catch (DataAccessException e) {
+                if (e.getMessage().equals("Invalid Credentials")) {
+                    res.status(401);
+                    return gson.toJson(new ErrorResponse("Error: Unauthorized"));
+                }
+                res.status(500);
+                return gson.toJson(new ErrorResponse("Error: " + e.getMessage()));
+            }
+        });
+        Spark.delete("/session", (req, res) -> {
+            String authToken = req.headers("Authorization");
+            if (authToken == null) {
+                res.status(401);
+                return gson.toJson(new ErrorResponse("Error: Unauthorized"));
+            }
+            try {
+                if (dataaccess.getAuth(authToken) == null){
+                    res.status(401);
+                    return gson.toJson(new ErrorResponse("Error: Unauthorized"));
+                }
+                Result result = sessionService.logout(authToken);
+                res.status(200);
+                return gson.toJson(new EmptyResponse());
+            } catch (DataAccessException e) {
+                res.status(401);
+                return gson.toJson(new ErrorResponse("Error: unauthorized"));
+            }
+        });
+        Spark.get("/game", (req, res) -> {
+            String authToken = req.headers("Authorization");
+            if (authToken == null) {
+                res.status(401);
+                return gson.toJson(new ErrorResponse("Error: Unauthorized"));
+            }
+            try {
+                GameListResult result = gameService.listGames(authToken);
+                res.status(200);
+                return gson.toJson(new GameListResponse(result.games()));
+            } catch (DataAccessException e) {
+                if (e.getMessage().equals("Unauthorized")) {
+                    res.status(401);
+                    return gson.toJson(new ErrorResponse("Error: Unauthorized"));
+                }
+                res.status(500);
+                return gson.toJson(new ErrorResponse("Error: " + e.getMessage()));
+            }
+        });
+        Spark.post("/game", (req, res) -> {
+            String authToken = req.headers("Authorization");
+            if (authToken == null) {
+                res.status(401);
+                return gson.toJson(new ErrorResponse("Error: Unauthorized"));
+            }
+            try {
+                CreateGameRequest request = gson.fromJson(req.body(), CreateGameRequest.class);
+                if (request == null || request.gameName == null) {
+                    res.status(400);
+                    return gson.toJson(new ErrorResponse("Error: Bad request"));
+                }
+                CreateGameResult result = gameService.createGame(authToken, request.gameName);
+                res.status(200);
+                return gson.toJson(result);
+            } catch (DataAccessException e) {
+                if (e.getMessage().equals("Unauthorized")) {
+                    res.status(401);
+                    return gson.toJson(new ErrorResponse("Error: Unauthorized"));
+                }
+                res.status(500);
+                return gson.toJson(new ErrorResponse("Error: " + e.getMessage()));
+            } catch (Exception e) {
+                res.status(500);
+                return gson.toJson(new ErrorResponse("Error: Bad request"));
+            }
+        });
+        Spark.put("/game", (req, res) -> {
+            String authToken = req.headers("Authorization");
+            if (authToken == null) {
+                res.status(401);
+                return gson.toJson(new ErrorResponse("Error: Unauthorized"));
+            }
+            try {
+                JoinGameRequest request = gson.fromJson(req.body(), JoinGameRequest.class);
+                if (request == null || request.gameID == null || request.playerColor == null) {
+                    res.status(400);
+                    return gson.toJson(new ErrorResponse("Error: Bad request"));
+                }
+                JoinGameResult result = gameService.joinGame(authToken, request.gameID, request.playerColor);
+                res.status(200);
+                return gson.toJson(new EmptyResponse());
+            } catch (DataAccessException e) {
+                if (e.getMessage().equals("Unauthorized")) {
+                    res.status(401);
+                    return gson.toJson(new ErrorResponse("Error: Unauthorized"));
+                } else if (e.getMessage().equals("Invalid Game") || e.getMessage().equals("Invalid Color")) {
+                    res.status(400);
+                    return gson.toJson(new ErrorResponse("Error: Bad request"));
+                } else if (e.getMessage().equals("Game full")) {
+                    res.status(403);
+                    return  gson.toJson(new ErrorResponse("Error: Game full"));
+                }
+                res.status(500);
+                return gson.toJson(new ErrorResponse("Error: " + e.getMessage()));
+            }
+        });
+        Spark.delete("/db", (req, res) -> {
+            try {
+                Result result = clearService.clear();
+                res.status(200);
+                return gson.toJson(new EmptyResponse());
+            } catch (DataAccessException e) {
+                res.status(500);
+                return gson.toJson(new ErrorResponse("Error: " + e.getMessage()));
+            }
+        });
+    }
+
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
     }
+
+    private record RegisterRequest(String username, String password, String email) {}
+
+    private record LoginRequest(String username, String password) {}
+
+    private record CreateGameRequest(String gameName) {}
+
+    private record JoinGameRequest(Integer gameID, String playerColor) {}
+
+    private record GameListResponse(GameData[] games) {}
+
+    private record ErrorResponse(String message) {}
+
+    private record EmptyResponse() {}
 }
