3feac0d4227023bdd609d47908837ca3a9cb54b9
Gonzalo Loayza <gloayza05@gmail.com>
2025-05-21 14:46:05 -0500

    Updates on Server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,29 @@
 package server;
 
+import dataaccess.MemoryDataAccess;
+import dataaccess.dataAccess;
+import service.*;
+import com.google.gson.Gson;
 import spark.*;
+import service.Results.*;
 
 public class Server {
+
+    private final dataAccess dataaccess;
+    private final UserService userService;
+    private final GameService gameService;
+    private final SessionService sessionService;
+    private final ClearService clearService;
+    private final Gson gson;
+
+    public Server() {
+        this.dataaccess = new MemoryDataAccess();
+        this.userService = new UserService(dataaccess);
+        this.gameService = new GameService(dataaccess);
+        this.sessionService = new SessionService(dataaccess);
+        this.clearService = new ClearService(dataaccess);
+        this.gson = new Gson();
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
