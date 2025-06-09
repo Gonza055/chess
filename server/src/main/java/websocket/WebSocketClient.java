623204300e8bdf2f042a5de1baf64807c465232e
Gonzalo Loayza <gloayza05@gmail.com>
2025-06-09 10:14:45 -0500

    Refactor WebSocketClient to support message handling and listener callbacks

--- server/src/main/java/websocket/WebSocketClient.java
+++ server/src/main/java/websocket/WebSocketClient.java
@@ -1,36 +1,51 @@
 package websocket;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
-import com.mysql.cj.xdevapi.Client;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+import websocket.commands.UserGameCommand;
 
 import java.io.IOException;
 import java.net.URI;
 
 public class WebSocketClient {
-    private Session session;
-    private Client client;
     private final Gson gson = new Gson();
+    private clientListener listener;
+    private WebSocketClient client;
+    private WebSocketClientEndpoint endpoint;
 
-    public WebSocketClient(Session session) {
-        this.client = client;
+    public interface clientListener {
+        void onGameUpdate(ChessGame game);
+        void onNotification(String message);
+        void onError(String errorMessage);
+    }
+
+    public WebSocketClient() {
+    }
+
+    public void setListener(clientListener listener) {
+        this.listener = listener;
     }
 
 
-    public void connect(String url, String authToken, String gameId) throws Exception {
-        WebSocketClient wsClient = new WebSocketClient();
-        wsClient.start();
-        URI uri = new URI(url + "?authToken=" + authToken + "&gameId=" + gameId);
-        wsClient.connect(this, uri);
-    }
-
-    @OnWebSocketConnect
-    public void onConnect(Session session) {
-        this.session = session;
-        System.out.println("Connected to WebSocket");
+    public void connect(String wsUrl, String authToken, String gameId) throws Exception {
+        if (client != null && client.isRunning()) {
+            disconnect();
+        }
+        client = new WebSocketClient();
+        endpoint = new WebSocketClientEndpoint();
+        client.start();
+        endpoint.setMessageHandler(message -> {
+            handleMessage(message);
+        });
+        URI uri = new URI(wsUrl);
+        ClientUpgradeRequest request = new ClientUpgradeRequest();
+        client.connect(endpoint, uri, request);
+        UserGameCommand connectCommand = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, Integer.parseInt(gameId));
     }
 
     @OnWebSocketMessage
