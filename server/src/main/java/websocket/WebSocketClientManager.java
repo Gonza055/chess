108cd53f0ff5a9555769a21039e1a5ebb8abb45f
Gonzalo Loayza <gloayza05@gmail.com>
2025-06-10 20:56:58 -0500

    Refactor wsClient name so as to not have issues with similar usages

--- server/src/main/java/websocket/WebSocketClientManager.java
+++ server/src/main/java/websocket/WebSocketClientManager.java
@@ -3,6 +3,7 @@ package websocket;
 import chess.ChessGame;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
 import websocket.commands.UserGameCommand;
 import websocket.messages.LoadGameMessage;
 import websocket.messages.ServerMessage;
@@ -11,12 +12,17 @@ import websocket.messages.ServerMessageError;
 import websocket.messages.ServerMessageNotification;
 
 import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
-public class WebSocketClient {
+public class WebSocketClientManager {
     private final Gson gson = new Gson();
     private clientListener listener;
     private WebSocketClient client;
     private WebSocketClientEndpoint endpoint;
+    private boolean isRunning = false;
+
+    private String currentAuthToken;
+    private Integer currentGameID;
 
     public interface clientListener {
         void onGameUpdate(ChessGame game);
@@ -24,7 +30,14 @@ public class WebSocketClient {
         void onError(String errorMessage);
     }
 
-    public WebSocketClient() {
+    public WebSocketClientManager() {
+        this.client = new WebSocketClient();
+        try {
+            client.start();
+            isRunning = true;
+        } catch (Exception e) {
+            System.err.println("Error starting WebSocketClient: " + e.getMessage());
+        }
     }
 
     public void setListener(clientListener listener) {
@@ -33,19 +46,19 @@ public class WebSocketClient {
 
 
     public void connect(String wsUrl, String authToken, String gameId) throws Exception {
-        if (client != null && client.isRunning()) {
-            disconnect();
-        }
-        client = new WebSocketClient();
-        endpoint = new WebSocketClientEndpoint();
-        client.start();
-        endpoint.setMessageHandler(message -> {
-            handleMessage(message);
-        });
+        this.currentAuthToken = authToken;
+        this.currentGameID = Integer.parseInt(gameId);
+
         URI uri = new URI(wsUrl);
+
+        endpoint = new WebSocketClientEndpoint();
+        endpoint.setMessageHandler(this::handleMessage);
+
         ClientUpgradeRequest request = new ClientUpgradeRequest();
-        client.connect(endpoint, uri, request);
-        UserGameCommand connectCommand = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, Integer.parseInt(gameId));
+
+        client.connect(endpoint, uri, request).get(5, TimeUnit.SECONDS);
+
+        sendCommand(new UserGameCommand(UserGameCommand.CommandType.CONNECT, currentAuthToken, currentGameID));
     }
 
     public void disconnect() {
